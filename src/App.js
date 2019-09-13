e01e58384e84545305c41185a9c13cd18b01afdf
Liam Dawson <liamrdawson@gmail.com>
2019-09-13 15:40:28 +0100

    Fetch JSON data from the Google Vision API. From this data, for each image reasponse, create an object containing URL, array of labels and ID.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import data from './data';
 
 //Styles
 import './scss/App.scss';
@@ -10,7 +11,78 @@ import ImageList from './components/ImageList';
 import Footer from './components/Footer';
 
 
+const images = data;
+//let filteredImages = images;
+//filteredImages.forEach(imagePath => getLabels(imagePath));
+
+const createRequestJSON = paths => ({
+  requests: paths.map(path => ({
+      image: {
+      source: {
+          // DO NOT CHANGE! this is where the demo images are hosted
+          imageUri: `gs://${path}`,
+      },
+      },
+      //REQUEST FEATURES FROM VISION API
+      features: [
+      {
+          "type":"LABEL_DETECTION",
+          "maxResults": 10
+      }
+      ]
+  }))
+});
+
+const getGoogleVisionUrl = () => {
+  return `https://vision.googleapis.com/v1/images:annotate?key=${process.env.REACT_APP_GOOGLE_VISION_API_KEY}`;
+};
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      images: []
+    }
+  }
+
+  buildDataObject = (str, arr) => {
+    this.setState(prevState => ({
+      images: [
+        ...prevState.images,
+        {
+          name: str,
+          labels: arr.map(obj => obj.description),
+          id: arr[0].mid
+        }
+      ]
+    }));
+  };
+
+
+
+
+  getLabels = (path) => {
+    const url = getGoogleVisionUrl();
+    fetch((url), {
+      method: 'POST',
+      body: JSON.stringify(createRequestJSON([path]))
+    }).then(response => response.json())
+      .catch((err) => { console.log('error!', err); })
+      .then(data => data.responses[0].labelAnnotations)
+      .then(arr => this.buildDataObject(path, arr));
+      // .then(final => 
+      //   this.setState([{
+      //       labels: final.map(obj => obj.description),
+      //       name: path,
+      //       id: final[0].mid
+      //     }])
+      // );
+  }
+
+  componentDidMount() {
+    images.forEach(index => this.getLabels(index));
+  }
 
   render () {
     return (
